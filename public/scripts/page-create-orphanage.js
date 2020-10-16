5eeb49227ecf7b7b3f0b5aabc0b42fa16ed52220
arsmcosta <arsmcosta@gmail.com>
2020-10-16 14:26:58 -0300

    dia 3
    add e remove de campos de imagem, toggle de sim e não para atendimento aos finais de semana

---
+++ public/scripts/page-create-orphanage.js
@@ -0,0 +1,89 @@
+//cerate map
+const map = L.map("mapid").setView([-27.222633, -49.6455874], 15)
+
+//create and add tileLayer
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map)
+
+//create icon
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68]
+})
+
+let marker;
+
+//create and add marker
+map.on('click', (event) => {
+    const lat = event.latlng.lat;
+    const lng = event.latlng.lng;
+
+    document.querySelector('[name=lat]').value = lat;
+    document.querySelector('[name=lng]').value = lng;
+
+    //remove icon
+    marker && map.removeLayer(marker);
+
+    //add icon layer
+    marker = L.marker([lat, lng], { icon })
+        .addTo(map)
+})
+
+//add photo field
+function addPhotoField() {
+    //get photo container #images
+    const container = document.querySelector('#images')
+
+    //pegar o container para duplicar
+    const fieldsContainer = document.querySelectorAll('.new-upload')
+
+    //realizar a colne d aúltima imagem adcioanda
+    const newFieldContainer = fieldsContainer[fieldsContainer.length - 1].cloneNode(true)
+
+    //verificar se o campo está vazio, se sim, não adicionar ao container de imagens
+    const input = newFieldContainer.children[0]
+
+    if (input.value == "") {
+        return
+    }
+
+    //limpar o campo antes de adicionar ao container de imagens
+    input.value = ""
+
+    //adicionar o clone ao container de #images
+    container.appendChild(newFieldContainer)
+}
+
+function deleteField(event) {
+    const span = event.currentTarget
+
+    const fieldsContainer = document.querySelectorAll('.new-upload')
+
+    if(fieldsContainer.length <= 1) {
+        span.parentNode.children[0].value = ""
+        return
+    }
+
+    //deletar o campo
+    span.parentNode.remove();
+}
+
+//selecionar sim e não
+function toggleSelect(){
+
+    //retirar a class .active (dos botoes)
+    document.querySelectorAll('.button-select button')
+    .forEach( function(button) {
+        button.classList.remove('active')
+    })
+
+    //colocar a class .active no botão clicado
+    const button = event.currentTarget
+    button.classList.add('active')
+
+    //atualizar o input hidden com o valor selecionado
+    const input = document.querySelector('[name="open_on_weekends"]')
+
+    input.value = button.dataset.value
+}
+    
