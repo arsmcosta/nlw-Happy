b21e46abdcdb5444f6312677946e16a0fdf48f12
arsmcosta <arsmcosta@gmail.com>
2020-10-16 19:09:37 -0300

    dia 4
    iniciando o backend com uma fakedb, hbs e node

---
+++ src/database/fakedata.js
@@ -0,0 +1,35 @@
+module.exports = [
+    {
+        id: 1,
+        lat: "-27.222633", 
+        lng: "-49.6455874",
+        name: "Lar das meninas",
+        description: "Preste assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        images: [
+
+            "https://source.unsplash.com/random?sig=10",
+
+            "https://source.unsplash.com/random?sig=11"
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas Das 08h até as 18h",
+        open_on_weekends: "1"
+    },
+
+    {
+        id: 2,
+        lat: "-27.222633", 
+        lng: "-49.6555874",
+        name: "Lar dos meninos",
+        description: "Preste assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+        images: [
+
+            "https://source.unsplash.com/random?sig=12",
+
+            "https://source.unsplash.com/random?sig=13"
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas Das 08h até as 18h",
+        open_on_weekends: "0"
+    }
+]
